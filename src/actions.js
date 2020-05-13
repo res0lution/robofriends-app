bca1fd813556406a379fd854fdb06ccb446b5830
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-13 09:48:01 +0300

    Created redux store

--- src/actions.js
+++ src/actions.js
@@ -2,5 +2,7 @@ import { CHANGE_SEARCH_FIELD } from "./constants";
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
-  payload: text,
+  payload: {
+    text,
+  },
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "tachyons";
 
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { App } from "./containers/App";
+import { searchRobots } from "./reducers";
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
