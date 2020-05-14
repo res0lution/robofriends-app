6f711ce4859590344208759276a220a8147f77a5
Bespolezniy <plotnickov.jaroslav@yandex.ru>
2020-05-14 09:50:44 +0300

    Updates

---
+++ precache-manifest.43821579950548d6bac7f66b38585818.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab99c656cd08f4c2bf3b9b51fa865c1",
+    "url": "/Res01uti0n/robofriends-app/index.html"
+  },
+  {
+    "revision": "0d6b5d9bc7d7d999b88c",
+    "url": "/Res01uti0n/robofriends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6728d4332c5b74fdb346",
+    "url": "/Res01uti0n/robofriends-app/static/css/main.110e2348.chunk.css"
+  },
+  {
+    "revision": "0d6b5d9bc7d7d999b88c",
+    "url": "/Res01uti0n/robofriends-app/static/js/2.ec91d74b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Res01uti0n/robofriends-app/static/js/2.ec91d74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6728d4332c5b74fdb346",
+    "url": "/Res01uti0n/robofriends-app/static/js/main.2e6b9b2c.chunk.js"
+  },
+  {
+    "revision": "fb5a27bb873cff8928db",
+    "url": "/Res01uti0n/robofriends-app/static/js/runtime-main.d8686139.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Res01uti0n/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
